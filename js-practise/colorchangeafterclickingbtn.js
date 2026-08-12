764581dbfdd14d704c586937af903838d1756448
Yujal <laxmikhulalbasnet@gmail.com>
2026-08-12 19:04:10 +0545

    feat: practising

---
+++ js-practise/colorchangeafterclickingbtn.js
@@ -0,0 +1,24 @@
+const btn1 = document.querySelector(".clickBtn1");
+const btn2 = document.querySelector(".clickBtn2");
+const div = document.querySelector(".container");
+
+btn1.addEventListener("click", () => {
+    
+  div.style.backgroundColor = "red";
+});
+
+btn2.addEventListener("click", () => {
+  div.style.backgroundColor = "blue";
+});
+
+btn3.addEventListener("click", () => {
+  div.style.backgroundColor = "green";
+});
+
+btn4.addEventListener("click", () => {
+  div.style.backgroundColor = "yellow";
+});
+
+btn5.addEventListener("click", () => {
+  div.style.backgroundColor = "white";
+});
