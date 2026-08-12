795030b3ceeddbc32667d083c0cad7a7b71f2bee
Yujal <laxmikhulalbasnet@gmail.com>
2026-08-12 21:09:32 +0545

    feat: Practising

---
+++ js-practise/boxopenafterclkickingbtn.js
@@ -0,0 +1,8 @@
+const btnn = document.querySelector(".btn1");
+const boxx = document.querySelector(".box");
+
+
+btn1.addEventListener("click", () => {
+    
+  div.style.backgroundColor = "red";
+});
