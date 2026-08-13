b3c479c2eaef45fc9e2c7a92c79845360d8b4170
Yujal <laxmikhulalbasnet@gmail.com>
2026-08-13 20:03:52 +0545

    Feat: Practising

--- js-practise/descriptionchng.js
+++ js-practise/descriptionchng.js
@@ -1,3 +1,7 @@
 const H = document.querySelector(".T");
 const C = document.querySelector(".S");
 const J = document.querySelector(".A");
+
+const one = document.querySelector(".text1");
+const two = document.querySelector(".text2");
+const three = document.querySelector(".text3");
