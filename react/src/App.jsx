c441ddca9319e4f1d29d3fd4ed5a7219b705124e
Yujal <laxmikhulalbasnet@gmail.com>
2026-08-25 19:41:00 +0545

    feat: Practising Tailwind

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="underline">HEllo</div>
+    <div class="container">
+
+        <h1>TODO LIST</h1>
+
+        <div class="search">
+            <input type="text" placeholder="Search"></input>
+            <button>Add</button>
+        </div>
+
+        {/* <!-- Boxes Container --> */}
+        <div class="title-container">
+
+            {/* <!-- First Box --> */}
+            <div class="title">
+
+                <h2>Title</h2>
+
+                <h3>Description</h3>
+
+                <h3>Priority</h3>
+
+                <div class="bttn">
+                    <button>Submit</button>
+                    <button>Cancel</button>
+                </div>
+
+            </div>
+
+            
+            <div class="title2">
+                
+            </div>
+
+        </div>
+
+    </div>
   )
 }
 
