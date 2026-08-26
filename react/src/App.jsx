ad589af75c7f2b1d0e38d9f3fd645c7b13049c8f
Yujal <laxmikhulalbasnet@gmail.com>
2026-08-26 20:52:26 +0545

    feat: Tailwind Practise

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
         <div class="flex justify-center items-center mt-[20px] py-[10px] px-[20px] ">
             <input class="w-1/2  border-none text-3xl bg-white" type="text" placeholder="Search"></input>
-            <button class="border-2 border-black text-3xl cursor-pointer py-[10px] px-[20px] bg-white hover:bg-white-100">Add</button>
+            <button class="border-1 border-black text-2xl cursor-pointer py-[10px] px-[20px] bg-white hover:bg-white-100">Add</button>
         </div>
 
         {/* <!-- Boxes Container --> */}
