5fe40d36db491820774d8a127fa14930d11936a8
YujalBasnet <laxmikhulalbasnet@gmail.com>
2026-09-07 21:54:41 +0545

    Update App.jsx

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -10,8 +10,6 @@ function App() {
             <input class="w-1/2  border-none text-3xl bg-white" type="text" placeholder="Search"></input>
             <button class="border-1 border-black text-2xl cursor-pointer py-[10px] px-[20px] bg-white rounded-lg hover:bg-white-100">Add</button>
         </div>
-
-       
         <div class="flex gap-[20px] mt-[20px] ml-[20px]">
 
             
