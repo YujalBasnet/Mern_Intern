181af04b3eb56d50228ff3a8c6fbca35648d2424
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-09 17:10:32 +0545

    feat: Update routing in App component to set Api_testing as the default route

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Todo from './components/todo';
 import Clone from './components/clone';
+import Api_testing from './components/Api_testing';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const App = () => {
 
     const router = createBrowserRouter([
       {
-        path: "/",
+        path: "/todo",
         element: <Todo />
       },
       {
         path : "/clone",
         element : <Clone />
+      },
+      {
+        path : "/",
+        element : <Api_testing />
       }
     ]);
 
