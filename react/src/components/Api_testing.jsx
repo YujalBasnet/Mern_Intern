1bdc1fd91a4bc1f85025aa202131a049bc5f9787
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-12 15:14:52 +0545

    feat: Add useNavigate import to Api_testing component for potential navigation functionality

--- react/src/components/Api_testing.jsx
+++ react/src/components/Api_testing.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Api_testing = () => {
     const [dataharu, setdataharu] = useState([]);
