bb18f2f9716f391e0ede5a3aac7810b69f8bd2bf
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-13 19:34:12 +0545

    feat: Add Registered_User component and route for displaying registered users

---
+++ react/src/components/Registered_User.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+
+function Registered_User() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+
+    const getUsers = async () => {
+
+      try {
+        const response = await fetch(
+           "https://jsonplaceholder.typicode.com/users"
+        );
+
+        const data = await response.json();
+
+        setUsers(data);
+
+      } catch (error) {
+        console.log("Error:", error);
+      }
+
+    };
+
+    getUsers();
+
+  }, []);
+
+  return (
+    <div>
+
+      <h1>Registered Users</h1>
+
+      {users.map((user) => (
+        <p key={user.id}>
+          {user.name}
+        </p>
+      ))}
+
+    </div>
+  );
+}
+
+export default Registered_User;
