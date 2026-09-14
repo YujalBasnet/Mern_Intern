b3172b6e1dfb6502a904085af58a3fa746a2a70e
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-14 15:13:34 +0545

    feat: Import necessary hooks and libraries for Product_details component

--- react/src/pages/Product_details.jsx
+++ react/src/pages/Product_details.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Product_details = () => {
   return (
