1afea41e6b4f21786fc2419a8ed62e4546fda1c5
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-14 15:19:26 +0545

    feat: Implement product fetching and loading state in Product_details component

--- react/src/pages/Product_details.jsx
+++ react/src/pages/Product_details.jsx
@@ -4,8 +4,32 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Product_details = () => {
-  return (
-    <div>Product_details</div>
+    const { id } = useParams();
+    const [product, setproduct] = useState({});
+    const [loading, setloading] = useState(false);
+
+    const fetchData = async () => {
+        setloading(true);
+        await axios.get(`https://fakestoreapi.com/products/${id}`)
+        .then((res) => {
+            setproduct(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+        setloading(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (loading) {
+        return <h1 className= "text-6xl text-center" >Loading...</h1>;
+    }
+
+    return (
+    <div className="flex felx-col justify-center items-center"></div>
   )
 }
 
