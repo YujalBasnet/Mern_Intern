a836d7f4720c8cb686fe9fc3d49142431a601f9d
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-20 13:30:37 +0545

    fix: add missing newline at the end of user.route.js for better formatting

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -4,7 +4,9 @@ import {getUser, postUser} from "../controllers/user.js";
 
 const route = express.Router();
 
+
 route.get("/user", getUser);
 route.post("/post-user",postUser);
+
 
 export default route;
