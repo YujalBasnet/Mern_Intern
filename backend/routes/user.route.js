ff0d76ff02842af781166a6a1769cd3a1645df50
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-22 17:21:48 +0545

    feat: add user-related routes for fetching, editing, and deleting users

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getUser, postUser} from "../controllers/user.js";
+import {getUser, postUser, getUserById, editUser, deleteUser} from "../controllers/user.js";
 
 
 const route = express.Router();
@@ -7,6 +7,8 @@ const route = express.Router();
 
 route.get("/user", getUser);
 route.post("/post-user",postUser);
-
+route.get("/getUserById/:id", getUserById);
+route.put("/editUser/:id", editUser);
+route.delete("/deleteUser/:id", deleteUser);
 
 export default route;
