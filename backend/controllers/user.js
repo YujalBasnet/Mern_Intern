01f0f975d6b48055d13ed3e07fd5ddb2f4e7e952
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-22 17:14:47 +0545

    refactor: clean up getUser function and remove commented code

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,16 +1,45 @@
 import database from "../database/database.js";
 
 export const getUser = (req, res) => {
-    const user={
-        name: "Yujal Khulal Basnet",
-        email: "yujal@gmail.com",
-        phone_number: "9800000000",
-        address: "Gothgaun, Morang",
-        role: "user",
-    };
-    console.log(user);
+    // const user={
+    //     name: "Yujal Khulal Basnet",
+    //     email: "yujal@gmail.com",
+    //     phone_number: "9800000000",
+    //     address: "Gothgaun, Morang",
+    //     role: "user",
+    // };
+    // console.log(user);
     
-    return res.send(user);
+    // return res.send(user);
+    try{
+        const q = "SELECT * FROM users";
+
+        database.query(q,(err, data) => {
+            if (err){
+                return res.send({message: "Error while fetching data", error: err,});
+            }
+            return res.send({message: "Data fetched successfully", data: data});
+        });
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export const getUserById = (req, res) => {
+    try{
+        const {id} = req.params;
+
+        const q = "SELECT * FROM users WHERE id = ?";
+
+        database.query(q, [id], (err, data) => {
+            if(err){
+                return res.status(500).send({message: "Error while fetching data", error: err,});
+            }
+            return res.status(200).send({message: "Data fetched successfully", data: data});
+        });
+    }catch(error){
+        console.log(error);
+    }
 };
 
 
