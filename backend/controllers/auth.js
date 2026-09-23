2110189a4474b240c7286b0fdfe14d5081dce936
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-23 14:02:45 +0545

    feat: enhance login function with user authentication logic and error handling

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,7 +3,31 @@ import bcrypt from "bcryptjs";
 
 export const login = (req, res) => {
     try{
+        const { email, password } = req.body;
 
+        const q = "SELECT * FROM users WHERE email = ?";
+
+        database.query(q, [email], (err, data) => {
+            if(err){
+                return res.status(500).send({message: "Error while fetching data", error: err});
+            }
+            if(data.length === 0){
+                return res.status(404).send({message: "User not found"});
+            }else{
+                const passwordMatch= bcrypt.compareSync(password, result[0].password);
+
+                if (passwordMatch){
+                    return res.status(200).send({
+                        message: "user login successfully",
+                        data: result[0],
+                    });
+                }else{
+                    return res.status(404).send({ message: "Enail or password didn't match",
+
+                    });
+                }
+            }
+        });
     }catch(error){
         console.log(error);
     }
