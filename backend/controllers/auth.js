4eb0c109646a868d5857e34a9fb1e9425fa5722b
Yujal <laxmikhulalbasnet@gmail.com>
2026-09-23 13:56:59 +0545

    feat: implement login function structure and update database connection

---
+++ backend/controllers/auth.js
@@ -0,0 +1,10 @@
+import database from "../database/database.js";
+import bcrypt from "bcryptjs";
+
+export const login = (req, res) => {
+    try{
+
+    }catch(error){
+        console.log(error);
+    }
+};
